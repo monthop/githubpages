c5b960a548c0e737be1a87d2b01fa24c63b18d9d
monthop <monthop@sumana.org>
2020-03-27 13:34:31 +0700

    test github pages

---
+++ README.md
@@ -0,0 +1,15 @@
+# wpcovid
+
+github pages simple
+    http://monthop.github.io/wpcovid
+
+github pages React
+    http://monthop.github.io/wpcovid/react/build
+
+howto
+    https://pages.github.com/
+    
+Limitations of GitHub Pages:
+    Repos must be less than 1 GB in file size.
+    Websites must be less than 1 GB in file size.
+    Monthly Bandwidth limit is 100 GB. We’ll bypass this later on.
---
+++ react/build/precache-manifest.679ec1d5c6b92fddf93f212d5d077bb2.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "369fc4cb31d5260d44488a241b48bf74",
+    "url": "./index.html"
+  },
+  {
+    "revision": "438ba43da54fb0f99463",
+    "url": "./static/css/2.9044517a.chunk.css"
+  },
+  {
+    "revision": "3b2c9fcb1ca03740f227",
+    "url": "./static/css/main.57f98f40.chunk.css"
+  },
+  {
+    "revision": "438ba43da54fb0f99463",
+    "url": "./static/js/2.342e0cc5.chunk.js"
+  },
+  {
+    "revision": "a6d74b0789c80ea7c4ab2a82879bb85e",
+    "url": "./static/js/2.342e0cc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b2c9fcb1ca03740f227",
+    "url": "./static/js/main.61063a57.chunk.js"
+  },
+  {
+    "revision": "8ffa459b4a3452beb650",
+    "url": "./static/js/runtime-main.8aa5386b.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "./static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "./static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "./static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "./static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "./static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
